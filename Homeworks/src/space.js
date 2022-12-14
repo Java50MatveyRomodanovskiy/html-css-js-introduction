79eef78f73ce7a00927a44187c44020bca216a80
MatveyRomodanovskiy <startpolus@gmail.com>
2022-12-14 15:53:15 +0300

    add js functions add and remove main image; add hide button, improve styles, fix bugs

--- Homeworks/src/space.js
+++ Homeworks/src/space.js
@@ -1,12 +1,22 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElements = document.querySelector("main");
+const hideButtonElement = document.querySelector("#hideCont-button")
+function showContainer(){
+    mainElements.classList.remove("hidden");
+}
+function hideContainer(){
+    mainElements.classList.add("hidden");
+}
 function setDetails(anchor) {
     detailsImage.src = anchor.getAttribute("data-details-image");
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    showContainer();
 }
 for (let i = 0; i < anchorElements.length; i++){
     anchorElements[i].addEventListener("click", function(){
         setDetails(anchorElements[i]);
     });
 }
+hideButtonElement.addEventListener("click",hideContainer);
