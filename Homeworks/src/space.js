8c6eb119125e244694429dacf331b41ac705583f
MatveyRomodanovskiy <startpolus@gmail.com>
2022-12-16 23:30:49 +0300

    add animation and sounds

--- Homeworks/src/space.js
+++ Homeworks/src/space.js
@@ -2,9 +2,19 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 const mainElements = document.querySelector("main");
-const hideButtonElement = document.querySelector("#hideCont-button")
+const hideButtonElement = document.querySelector("#hideCont-button");
+const IS_POINT = "is-point";
+const detailsContainer = document.querySelector(".details-container");
+const audioElements = document.getElementById("audio");
 function showContainer(){
     mainElements.classList.remove("hidden");
+    detailsContainer.classList.add(IS_POINT);
+    audioElements.play();
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT);
+    },2);
+    setTimeout(function(){audioElements.pause();},4000);
+    audioElements.currentTime = 0;
 }
 function hideContainer(){
     mainElements.classList.add("hidden");
@@ -12,6 +22,8 @@ function hideContainer(){
 function setDetails(anchor) {
     detailsImage.src = anchor.getAttribute("data-details-image");
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    audioElements.src = anchor.getAttribute("audio-src");
+    audioElements.currentTime = 0;
     showContainer();
 }
 for (let i = 0; i < anchorElements.length; i++){
